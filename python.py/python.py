421fa6f1c0e6713cdd71f689c76301c4c4d057de
Whyzow <wizoremainrb@gmail.com>
2025-02-11 07:29:29 +0000

    Submit

---
+++ python.py/python.py
@@ -0,0 +1,9 @@
+# Define distance and time
+distance = float(input("Insert distance in km: "))
+time = float(input("Insert time in seconds: ")); 
+ 
+# Define velocity
+velocity = distance/time
+
+# Print result here
+print(f"The velocity is {velocity:.2f} km/s")
